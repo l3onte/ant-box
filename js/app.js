e90224d0a923978b2654e34a1b5c4f59659c83fb
l3onte <l3ontejoelc@gmail.com>
2025-07-08 21:08:07 -0600

    feat: add sort functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 import * as ui from './ui.js';
 
 ui.showMovements();
-ui.openBudgetModal();
 ui.openMovementModal();
+ui.openBudgetModal();
 ui.closeModal();
+
 ui.showOptions();
 ui.openOptions();
+ui.sortByid();
+ui.sortByAlph();
