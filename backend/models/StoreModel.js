6e1449e490f3f5ab8d4be29f14df53e16e7a7afa
l3onte <l3ontejoelc@gmail.com>
2025-11-10 00:33:51 -0600

    feat(sidebar): add user role feature

--- backend/models/StoreModel.js
+++ backend/models/StoreModel.js
@@ -14,6 +14,30 @@ const Store = {
         `, [id]);
 
         return rows[0];
+    },
+
+    getStoreByUser: async (id_usuario) => {
+        try {
+            const [ownerStore] = await db.query(`
+                SELECT * FROM Tiendas WHERE id_usuario = ?;
+            `, [id_usuario]);
+
+            if (ownerStore.length > 0) return ownerStore[0];
+
+            const [vendorStore] = await db.query(`
+                SELECT t.*
+                FROM Tiendas t
+                INNER JOIN Vendedores v ON v.id_tienda = t.id_tienda
+                WHERE v.id_usuario = ?;
+            `, [id_usuario]);
+
+            if (vendorStore.length > 0) return vendorStore[0];
+
+            return null;
+        } catch (error) {
+            console.error("Error en getStoreByUser:", error);
+            throw error;
+        }
     }
 }
 
--- backend/routes/storeRoutes.js
+++ backend/routes/storeRoutes.js
@@ -4,5 +4,6 @@ import StoreController from '../controllers/StoreController.js';
 const route = express.Router();
  
 route.get('/getStoreById/:id', StoreController.getStoreById);
+route.get('/getStoreByUser/:id', StoreController.getStoreByUser);
 
 export default route;
